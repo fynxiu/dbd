f3bece484727fe17a6491510f28c5d97bd398bff
fyn <fyn.xiu@gmail.com>
2022-04-14 20:08:10 +0800

    build: git get tag use --abbrev

--- hack/vcs.sh
+++ hack/vcs.sh
@@ -5,12 +5,8 @@ set -o pipefail
 
 # dbd::vcs::version - Print the version of the current VCS
 function dbd::vcs::version {
-    # Get the version name:
-    # 1) try tag-like version
-    # 2) try branch name
-    # 3) try name-rev (tag~<rev> or branch~<rev>)
     local version
-    version=$(command git describe --tags HEAD 2>/dev/null) \
+    version=$(command git describe --tags --abbrev=0 HEAD 2>/dev/null) \
     || version="v1.0.0-alpha.1"
     echo "${version}"
 }
